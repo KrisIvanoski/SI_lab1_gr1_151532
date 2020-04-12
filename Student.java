d7a210b6e88e5cd75ec9e2bd17f8d126c760a3cd
Кристијан Иваноски <zeroivanoski@gmail.com>
2020-04-12 22:53:59 +0200

    student class finished

--- Student.java
+++ Student.java
@@ -4,14 +4,44 @@ class Student {
 	String lastName;
 
 	//TODO constructor
+	public Student(index, firstName, lastName){
+		this.index = index;
+		this.firstName = firstName;
+		this.lastName = lastName;
+
+	}
 
 	//TODO seters & getters
+	public void setName(String firstName){
+		this.firstName = firstName;
+	}
+	public void setIndex(String index){
+		this.index = index;
+	}
+	public void lastName(String lastName){
+		this.lastName = lastName;
+	}
+
+	public String lastName(){
+		return lastName;
+	}
+	public String firstName(){
+		return firstName;
+	}
+	public String index(){
+		return index;
+	}
 
 	public double getAverage() {
 		//TODO
+		// nemam idea kako da go napravam toa bidejki nemam od kade da vidam poeni zatoa sekoj ke ima poeni -1 :)
+		
+		return -1;
 	}
 
 	public boolean hasSignature() {
 		//TODO
+		// nemam idea kako da go napravam toa bidejki nemam od kade da vidam lab taka da sekoj ke ima potpis :)
+		return true;
 	}
 }
